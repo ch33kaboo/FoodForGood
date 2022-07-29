3770fb54a4ba7ea6f71b263bbc3c9d8bf7d12d5d
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-07-29 02:14:40 +0100

    styled the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
         102: '1.02'
       },
       animation: {
-        blob: "blob 7s infinite"
+        blob: "blob 7s infinite",
+        chkbo: "chkbo 6s infinite"
       },
       keyframes: {
         blob: {
@@ -26,6 +27,23 @@ module.exports = {
           },
           "100%": {
             transform: "translate(0px, 0px) scale(1)"
+          }
+        },
+        chkbo: {
+          "0%": {
+            color: "rgb(34 197 94 / var(--tw-border-opacity))"
+          },
+          "25%": {
+            color: "rgb(94 234 212 / var(--tw-bg-opacity))"
+          },
+          "50%": {
+            color: "rgb(34 197 94 / var(--tw-bg-opacity))"
+          },
+          "75%": {
+            color: "rgb(252 211 77 / var(--tw-bg-opacity))"
+          },
+          "100%": {
+            color: "rgb(34 197 94 / var(--tw-border-opacity))"
           }
         }
       }
